1dc2ce003d318b00149ed3c5122b2a5a91167755
Aditya Ranjan Pradhan <99034778+aditya8725@users.noreply.github.com>
2024-03-20 15:31:39 +0530

    Add files via upload

---
+++ 03-angular-component/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Welcome2Component } from './welcome2/welcome2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    Welcome2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 03-angular-component/src/app/welcome2/welcome2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"welcome-back",
+    template: "<p>Welcome2 Works</p>"
+})
+
+export class Welcome2Component
+{
+
+}
