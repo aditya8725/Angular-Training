6149adadbf792a9bd81b0c1fb8243ecd41e568b9
Aditya Ranjan Pradhan <99034778+aditya8725@users.noreply.github.com>
2024-03-20 15:39:21 +0530

    Add files via upload

---
+++ 07-student-appservice/src/app/student/student.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StudentService 
+{
+  students: any[]=[
+      {studentId: 101, studentName:'Aditya', studentScore:91},
+      {studentId: 102, studentName:'Vikash', studentScore:70},
+      {studentId: 103, studentName:'Shubham', studentScore:82},
+      {studentId: 104, studentName:'Akash', studentScore:81},
+      {studentId: 105, studentName:'Shreyansh', studentScore:87},
+    ];
+
+  constructor() { }
+
+  addStudent(student:any)
+  {
+    this.students.push(student)
+  }
+
+  getAllStudent()
+  {
+    return this.students;
+  }
+}
